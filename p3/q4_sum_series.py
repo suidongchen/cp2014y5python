614f3999e405e8a27f55c7b53df1cd9c6fa24204
suidongchen <suidongchen@users.noreply.github.com>
2014-01-23 23:03:21 +0800

    Update q4_sum_series.py

--- p3/q4_sum_series.py
+++ p3/q4_sum_series.py
@@ -1,10 +1,10 @@
 def m_series(i):
     i=float(i)
     if i==1:
-        return 1/2
+        return 0.5000
     else:
         return m_series(i-1) + i/(i+1)
 
 print "i         m(i)     "
 for i in range(1,21):
-    print " "*(10-len(i))+str(i)+"{0:.4f}".format(m_series(i))
+    print str(i)+" "*(10-len(str(i)))+"{0:.4f}".format(m_series(i))
