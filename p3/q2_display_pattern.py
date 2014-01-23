1d4d381f4199b08c8c71edfb3fbab9f13f40fb87
suidongchen <suidongchen@users.noreply.github.com>
2014-01-23 22:59:21 +0800

    Update q2_display_pattern.py

--- p3/q2_display_pattern.py
+++ p3/q2_display_pattern.py
@@ -1,8 +1,25 @@
 def display_pattern(n):
-    x=""
-    if 1<=n<=9:    
-        for i in range(1,n+1):
-            for j in range(i,0,-1):
-                x=x+str(j)+" "
-            space = (2n + 2i - 4)*" "
-        print space + x
+    lst=[]
+    lst_last=[]
+    for p in range(1,n+1):
+        p=str(p)
+        lst_last.append(p)
+    lst_last.reverse()
+    last=" ".join(lst_last)
+    k=len(last)
+
+    
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            j=str(j)
+            lst.append(j)
+        lst.reverse()
+        " ".join(lst)
+        print "{0:>20s}".format(lst)
+        lst=[]
+
+
+
+
+n = input("Enter a number: ")
+display_pattern(n)
